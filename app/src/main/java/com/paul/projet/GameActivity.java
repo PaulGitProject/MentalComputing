2c58f7e80f1a0eab07fb353016c447e3371f1147
Shayzette <29106016+ShayzerGit@users.noreply.github.com>
2022-03-09 18:23:42 +0100

    Mauvais push avant...3

--- app/src/main/java/com/paul/projet/GameActivity.java
+++ app/src/main/java/com/paul/projet/GameActivity.java
@@ -11,11 +11,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.paul.projet.models.OperationModel;
+import com.paul.projet.models.exceptions.OperatorException;
 import com.paul.projet.services.OperationService;
+import com.paul.projet.services.VerifyUserResultService;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView operationTextView;
+    private EditText resultEditText;
+    private OperationModel operationModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +38,48 @@ public class GameActivity extends AppCompatActivity {
         // Set BackgroundDrawable
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
 
+        resultEditText = findViewById(R.id.answer_input);
+        operationTextView = findViewById(R.id.calculation_text);
+
         OperationService operationService = new OperationService();
-        OperationModel operationModel = operationService.generateRandomOperation();
+        operationModel = operationService.generateRandomOperation();
+
+        String calcul = operationModel.getFirstValue() + " " + operationModel.getOperator() + " " + operationModel.getSecondValue();
+        operationTextView.setText(calcul);
+
+        Button validateResult = findViewById(R.id.validate_button);
+
+        validateResult.setOnClickListener(view -> {
+            try {
+                onValidateClick();
+            } catch (OperatorException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void onValidateClick() throws OperatorException {
+        int result = Integer.parseInt(resultEditText.getText().toString());
+        verifyResult(result);
+    }
+
+
+
+    private void verifyResult(int result) throws OperatorException {
+            VerifyUserResultService verifyUserResultService = new VerifyUserResultService();
+
+            boolean verifyResult = verifyUserResultService.verifyComputeResult(operationModel, result);
+
+            if(verifyResult){
+                TextView operatorTextViewCorrect = findViewById(R.id.correct_result_text);
+                operatorTextViewCorrect.setVisibility(View.VISIBLE);
+                TextView operatorTextViewIncorrect = findViewById(R.id.incorrect_result_text);
+                operatorTextViewIncorrect.setVisibility(View.GONE);
+                this.resultEditText.setVisibility(View.GONE);
+            }else{
+                TextView operatorTextViewIncorrect = findViewById(R.id.incorrect_result_text);
+                operatorTextViewIncorrect.setVisibility(View.VISIBLE);
+            }
     }
 
 
